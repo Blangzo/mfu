85edf14961b2b7b732948c857b4550e2d9d6f7fd
Wesley <wh395@cornell.edu>
2021-05-18 20:42:04 -0400

    mmfucontribute@protonmail.com

--- contact.php
+++ contact.php
@@ -35,7 +35,7 @@
   <div class="mb-4 mt-4">
       <div class="mb-2 mt-3 text-center">
         <p>General Questions: <a href="mailto:cdmpowermm@gmail.com">cdmpowermm@gmail.com</a></p>
-        <p>Content Contact: <a href="mailto:mmfucontent@protonmail.com">mmfucontent@protonmail.com</a></p>
+        <p>Content Contact: <a href="mailto:mmfucontribute@protonmail.com">mmfucontribute@protonmail.com</a></p>
         <p>Tech Contact: <a href="mailto:mmfutech@gmail.com">mmfutech@gmail.com</a></p>
       </div>
   </div>
