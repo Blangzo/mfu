dc8c9d975c639fe61ec4edaad516ee556b183699
Wesley <wh395@cornell.edu>
2021-05-28 00:07:37 -0400

    table -> folders

--- catalogue.php
+++ catalogue.php
@@ -8,12 +8,14 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <!-- <link rel="stylesheet" type="text/css" href="styles/v1.css"/> -->
-  <link rel="stylesheet" type="text/css" href="styles/v2.css"/>
+  <link rel="stylesheet" type="text/css" href="styles/catalogue.css"/>
   <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="lib/bootstrap/dist/css/open-iconic-bootstrap.min.css" />
   <link rel="stylesheet" href="styles/site.css" />
   <link rel="stylesheet" href="styles/efa.css" />
+  <script src="lib/jquery/dist/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="styles/topnav.css"/>
+  <script src="js/catalogue.js"></script>
 </head>
 
 <body>
@@ -33,21 +35,29 @@
      <h2 class="title-color mb-3 text-center">ဖက်ဒရယ် တက္ကသိုလ်</h2>
      <h2 class="title-color mb-2 text-center">Virtual Federal University</h2>
  </div>
-  <hr />
-  <div class="table-responsive">
-    <table id="course-table" class="display table table-striped cell-border">
- 		<thead class="thead-light">
+ <hr />
+<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Descriptions</th>
+				<th>File Type</th>
+				<th>File Size</th>
+      <th>Language</th>
+				<th>Download Link</th>
+			</tr>
+		</thead>
+		<tbody>
+    <tbody class="labels">
  			<tr>
- 				<th>Name</th>
- 				<th>Descriptions</th>
- 				<th>File Type</th>
- 				<th>File Size</th>
-         <th>Language</th>
- 				<th>Download Link</th>
+        <td colspan="6">
+        <label for="race_and_caste">Race and Caste</label>
+        <input type="checkbox" name="race_and_caste" id="race_and_caste" data-toggle="toggle">
+        </td>
  			</tr>
- 		</thead>
- 		<tbody>
- 			<tr>
+    </tbody>
+    <tbody class="hide">
+      <tr>
  				<td>Race and Caste</td>
  				<td>...</td>
  				<td>Zip</td>
@@ -55,7 +65,7 @@
          <td>EN & MY</td>
  				<td><a href="https://ipfs.io/ipfs/QmbRmApFWNvwpMQNeRsUbCGcL3MfJoxEdtmM34CbMQcfek?filename=Race_and_Caste.zip">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>Caste Matters by Suraj Yengde (z-lib.org)</td>
  				<td>part of the Race and Caste zip file</td>
  				<td>PDF</td>
@@ -63,7 +73,7 @@
          <td>EN</td>
  				<td><a href="https://ipfs.io/ipfs/QmaHXtUxpDNs1LCut5ZJiVbpDT52RciCHgid4EM1W34xdj?filename=Caste%20Matters%20by%20Suraj%20Yengde%20(z-lib.org).pdf">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>loomba-race_and_the_possibilities</td>
  				<td>part of the Race and Caste zip file</td>
  				<td>PDF</td>
@@ -71,7 +81,7 @@
          <td>EN</td>
  				<td><a href="https://ipfs.io/ipfs/Qmc8VYzY4VdjZAKAtB2PwipGoFuWA3GiDmBfCeisUvWF7e?filename=loomba-race_and_the_possibilities.pdf">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>Race and Caste 1 - audio</td>
  				<td>part of the Race and Caste zip file</td>
  				<td>MP3</td>
@@ -79,7 +89,7 @@
          <td>EN</td>
  				<td><a href="https://ipfs.io/ipfs/QmUQJqWrqzstTT4YZHGx4eH5t56rbCaP8Lq6Bt2zHtEGaq?filename=Race%20and%20Caste%201%20-%20audio.mp3">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>Race and Caste 1 - transcript.docx</td>
  				<td>part of the Race and Caste zip file</td>
  				<td>PDF</td>
@@ -87,7 +97,7 @@
          <td>EN</td>
  				<td><a href="https://ipfs.io/ipfs/QmPcPvBChrSaCdPAspu6GsdjqA85nhrvzNzGh3Zc2TAn3N?filename=Race%20and%20Caste%201%20-%20transcript.docx.pdf">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>The Intimacies of Four Continents by Lisa Lowe (z-lib.org)</td>
  				<td>part of the Race and Caste zip file</td>
  				<td>PDF</td>
@@ -95,7 +105,7 @@
          <td>EN</td>
  				<td><a href="https://ipfs.io/ipfs/QmYCBnW2XDCGNsGDpE1tGk93DX4z5DNWS4c14RG3XyLm6c?filename=The%20Intimacies%20of%20Four%20Continents%20by%20Lisa%20Lowe%20(z-lib.org).pdf">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>Race-and-Caste-1- Burmese Translation</td>
  				<td>part of the Race and Caste zip file</td>
  				<td>PDF</td>
@@ -103,15 +113,25 @@
          <td>MY</td>
  				<td><a href="https://ipfs.io/ipfs/Qmdhjrid7wGNX2iCGSWYXyVw2nb9rx4BZFPupq6vkYAXQL?filename=Race-and-Caste-1-%20Burmese%20Translation.pdf">Download link</a></td>
  			</tr>
+    </tbody>
+    <tbody class="labels">
+      <tr>
+        <td colspan="6">
+          <label for="critical_readings">Critical Readings</label>
+				  <input type="checkbox" name="critical_readings" id="critical_readings" data-toggle="toggle">
+        </td>
+      </tr>
+    </tbody>
+    <tbody class="hide">
  			<tr>
  				<td>Critical Readings</td>
  				<td>...</td>
  				<td>Zip</td>
  				<td>16.1 MB</td>
-         <td>EN</td>
+         <td>EN & MY/td>
  				<td><a href="https://ipfs.io/ipfs/QmXovPtcoV5wB2rakmZ6ebuhLvhSRjEQSrbDaqjnKtuhax?filename=Critical_Reading.zip">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>Critical Reading.mp3</td>
  				<td>part of the critical reading zip file</td>
  				<td>MP3</td>
@@ -119,7 +139,7 @@
          <td>MY</td>
  				<td><a href="https://ipfs.io/ipfs/QmfVV1DZP4a8CeMFBFgS82dEM38vcumCZT7wSZW6o7xxwT?filename=Critical%20reading.mp3">Download link</a></td>
  			</tr>
-       <tr>
+      <tr>
  				<td>Introduction to Critical Reading</td>
  				<td>part of the critical reading zip file</td>
  				<td>PDF</td>
@@ -127,6 +147,16 @@
          <td>EN</td>
  				<td><a href="https://ipfs.io/ipfs/QmYzdbhCbZEN7unDuTfUGsagBJ9sQ9Ngihk8vrmuXpbvcn?filename=Introduction%20to%20Critical%20reading%20.pdf">Download link</a></td>
  			</tr>
+    </tbody>
+    <tbody class="labels">
+			<tr>
+				<td colspan="6">
+					<label for="SEAsia_Cold_War">SEAsia Cold War</label>
+					<input type="checkbox" name="SEAsia_Cold_War" id="SEAsia_Cold_War" data-toggle="toggle">
+				</td>
+			</tr>
+    </tbody>
+    <tbody class="hide">
  			<tr>
  				<td>SEAsia Cold War</td>
  				<td>...</td>
@@ -135,41 +165,41 @@
          <td>EN & MY</td>
  				<td><a href="https://ipfs.io/ipfs/QmSFyejY4AeLW4zP1sxf8KsdLi4Rid5SQ9epojZ9srQgLr?filename=SEAsia_Cold_War.zip">Download link</a></td>
  			</tr>
-       <tr>
- 				<td>Political_Elites_in_Colonial_SEAsia</td>
- 				<td>part of the SEAsia Cold War zip file</td>
- 				<td>PDF</td>
- 				<td>2 MB</td>
-         <td>EN</td>
- 				<td><a href="https://ipfs.io/ipfs/QmXCpLqvS7oFrVgxDA6jP3X1zmgSPSuFvRkcscsAnoJovm?filename=Political_Elites_in_Colonial_SEAsia.pdf">Download link</a></td>
- 			</tr>
-       <tr>
- 				<td>communism-and-nationalism-in-burma</td>
- 				<td>part of the SEAsia Cold War zip file</td>
- 				<td>PDF</td>
- 				<td>336 KB</td>
-         <td>EN</td>
- 				<td><a href="https://ipfs.io/ipfs/QmPSfqYHyBmbGXfKwXcYr3GdM9NqhLrzkrsgExuLSho9V8?filename=communism-and-nationalism-in-burma.pdf">Download link</a></td>
- 			</tr>
-       <tr>
- 				<td>Cold_war_SEAsia_1</td>
- 				<td>part of the SEAsia Cold War zip file</td>
- 				<td>PDF</td>
- 				<td>795 KB</td>
-         <td>MY</td>
- 				<td><a href="https://ipfs.io/ipfs/QmPSfqYHyBmbGXfKwXcYr3GdM9NqhLrzkrsgExuLSho9V8?filename=communism-and-nationalism-in-burma.pdf">Download link</a></td>
- 			</tr>
-       <tr>
- 				<td>Cold_war_SEAsia_1 (online-audio-converter.com).mp3</td>
- 				<td>part of the SEAsia Cold War zip file</td>
- 				<td>MP3</td>
- 				<td>8 MB</td>
-         <td>MY</td>
- 				<td><a href="https://ipfs.io/ipfs/QmUTngUWXQwjehSaXPWWfS7hj8L4PUNBZfyn2kDMkyLY5K?filename=Cold_war_SEAsia_1%20(online-audio-converter.com).mp3">Download link</a></td>
- 			</tr>
- 		</tbody>
-     </table>
-  </div>
+      <tr>
+      	<td>Political_Elites_in_Colonial_SEAsia</td>
+      	<td>part of the SEAsia Cold War zip file</td>
+      	<td>PDF</td>
+      	<td>2 MB</td>
+       <td>EN</td>
+      	<td><a href="https://ipfs.io/ipfs/QmXCpLqvS7oFrVgxDA6jP3X1zmgSPSuFvRkcscsAnoJovm?filename=Political_Elites_in_Colonial_SEAsia.pdf">Download link</a></td>
+      </tr>
+      <tr>
+      	<td>communism-and-nationalism-in-burma</td>
+      	<td>part of the SEAsia Cold War zip file</td>
+      	<td>PDF</td>
+      	<td>336 KB</td>
+       <td>EN</td>
+      	<td><a href="https://ipfs.io/ipfs/QmPSfqYHyBmbGXfKwXcYr3GdM9NqhLrzkrsgExuLSho9V8?filename=communism-and-nationalism-in-burma.pdf">Download link</a></td>
+      </tr>
+      <tr>
+      	<td>Cold_war_SEAsia_1</td>
+      	<td>part of the SEAsia Cold War zip file</td>
+      	<td>PDF</td>
+      	<td>795 KB</td>
+       <td>MY</td>
+      	<td><a href="https://ipfs.io/ipfs/QmPSfqYHyBmbGXfKwXcYr3GdM9NqhLrzkrsgExuLSho9V8?filename=communism-and-nationalism-in-burma.pdf">Download link</a></td>
+      </tr>
+      <tr>
+      	<td>Cold_war_SEAsia_1 (online-audio-converter.com).mp3</td>
+      	<td>part of the SEAsia Cold War zip file</td>
+      	<td>MP3</td>
+      	<td>8 MB</td>
+       <td>MY</td>
+      	<td><a href="https://ipfs.io/ipfs/QmUTngUWXQwjehSaXPWWfS7hj8L4PUNBZfyn2kDMkyLY5K?filename=Cold_war_SEAsia_1%20(online-audio-converter.com).mp3">Download link</a></td>
+      </tr>
+     </tbody>
+   </tbody>
+ </table>
 
 </body>
 </html>
