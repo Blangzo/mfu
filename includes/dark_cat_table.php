e3b166509d624c8264103f41132647718913314f
Wesley <wh395@cornell.edu>
2021-06-11 10:01:10 -0400

    more dark mode fix

---
+++ includes/dark_cat_table.php
@@ -0,0 +1,22 @@
+<table>
+		<thead>
+			<tr>
+				<th>Course Name</th>
+				<th>Instructor</th>
+			</tr>
+		</thead>
+		<tbody>
+    	<tr>
+				<td><a href="catalogue/dark_race_and_caste.php">Race and Caste</a></td>
+				<td>Name Withheld</td>
+			</tr>
+			<tr>
+				<td><a href="catalogue/dark_critical_readings.php">Critical Readings</a></td>
+				<td>Name Withheld</td>
+			</tr>
+			<tr>
+				<td><a href="catalogue/dark_seasia_cold_war.php">SEAsia Cold War</a></td>
+				<td>Name Withheld</td>
+			</tr>
+   </tbody>
+ </table>
