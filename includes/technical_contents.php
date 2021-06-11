36b0c1ed0a6b44d7aeb1396c32ac1cfe998b8bfb
Wesley <wh395@cornell.edu>
2021-06-11 06:27:36 -0400

    changes to technical pg, moved catalog include to include folder (hopefully) left aligned technical <p>s, moved to an include, added spacing between sections, linked VPNs

---
+++ includes/technical_contents.php
@@ -0,0 +1,27 @@
+<div class="mb-2 mt-2">
+    <h2 class="title-color mb-3 text-center">ဖက်ဒရယ် တက္ကသိုလ်</h2>
+    <h2 class="title-color mb-2 text-center">Virtual Federal University</h2>
+</div>
+<div class="mb-4 mt-4">
+    <div class="mb-2 mt-3">
+      <h3 class="technical">VPN Use</h3>
+      <p>We highly recommend that you use a VPN when accesing this website or downloading any of our materials for your own privacy and safety. Some suggested free VPNs include:</p>
+      <ul>
+        <li><a href="https://protonvpn.com/">Proton VPN</a></li>
+        <li><a href="https://windscribe.com/">Windscibe Free</a></li>
+        <li><a href="https://www.tunnelbear.com/">TunnelBear</a></li>
+        <li><a href="https://hide.me/en/pricing">Hide.me Free</a></li>
+        <li><a href="https://www.avira.com/en/free-vpn">Avira Free VPN</a></li>
+      </ul>
+      <p>Higher quality VPN options exist. These provide unlimited data use so you don't have to worry about running out, but aren't necessary (try switching between free VPNs). Two suggestions in addition to paid options of the free suggestions above include <a href="https://nordvpn.com/">Nord VPN</a> and <a href="https://www.expressvpn.com/">Express VPN</a></p>
+
+      <h3 class="technical">Unzipping Files</h3>
+      <p>On most android devices, <a href="https://support.google.com/files/answer/9048509?hl=en">Files by Google</a> should be able to help you unzip files to access the content.</p>
+      <p>On some huawei phones, google services may be unavailable, however Huawei built-in files app should also be able to help you unzil files.</p>
+
+      <h3 class="technical">Sharing Files</h3>
+      <p>We enocourage students to share our files with anyone and everyone. Zapya is a free app that can help with file transfers between phones. Other options include passing on USB sticks or SD cards. SD cards can also be hidden inside some phones and thrown away if needed.</p>
+
+      <p>Tech Contact: <a href="mmfutech@gmail.com">mmfutech@gmail.com</a></p>
+    </div>
+</div>
--- technical.php
+++ technical.php
@@ -29,35 +29,7 @@
   <div class="alert alert-warning mb-3">
             <span class="oi oi-warning"></span>
             We highly recommend that you use a VPN when accessing this website or downloading any of our materials for your own privacy and safety. Please see below for a list of VPN suggetions. The VFU will never ask for your private information or for fees or donations from studets. If you receive requests like this please let us know.
- </div>
-  <div class="mb-2 mt-2">
-      <h2 class="title-color mb-3 text-center">ဖက်ဒရယ် တက္ကသိုလ်</h2>
-      <h2 class="title-color mb-2 text-center">Virtual Federal University</h2>
   </div>
-  <div class="mb-4 mt-4">
-      <div class="mb-2 mt-3 text-center">
-        <h3>VPN Use</h3>
-        <p>We highly recommend that you use a VPN when accesing this website or downloading any of our materials for your own privacy and safety. Some suggested free VPNs include:</p>
-        <ul>
-          <li>Proton VPN</li>
-          <li>Turbo VPN</li>
-          <li>1.1.1.1</li>
-          <li>Windscibe Free</li>
-          <li>TunnelBear</li>
-          <li>Hide.me Free</li>
-          <li>Avira Free VPN</li>
-        </ul>
-        <p>Higher quality VPN options exist. These provide unlimited data use so you don't have to worry about running out, but aren't necessary (try switching between free VPNs). Two suggestions are Express VPN and Nord VPN</p>
-
-        <h3>Unzipping Files</h3>
-        <p>On most android devices, <a href="https://support.google.com/files/answer/9048509?hl=en">Files by Google</a> should be able to help you unzip files to access the content.</p>
-        <p>On some huawei phones, google services may be unavailable, however Huawei built-in files app should also be able to help you unzil files.</p>
-
-        <h3>Sharing Files</h3>
-        <p>We enocourage students to share our files with anyone and everyone. Zapya is a free app that can help with file transfers between phones. Other options include passing on USB sticks or SD cards. SD cards can also be hidden inside some phones and thrown away if needed.</p>
-
-        <p>Tech Contact: <a href="mmfutech@gmail.com">mmfutech@gmail.com</a></p>
-      </div>
-  </div>
+  <?php include 'includes/technical_contents.php';?>
 </body>
 </html>
