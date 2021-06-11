2dddd639d800d70a00c3ce6aa46f4e73de0598e0
Wesley <wh395@cornell.edu>
2021-06-11 09:29:58 -0400

    filing

--- catalogue/race_and_caste.php
+++ catalogue/race_and_caste.php
@@ -8,25 +8,25 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <!--<link rel="stylesheet" type="text/css" href="styles/v1.css"/>-->
-  <link rel="stylesheet" type="text/css" href="styles/v2.css"/>
-  <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="lib/bootstrap/dist/css/open-iconic-bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="../styles/v2.css"/>
+  <link rel="stylesheet" href="../lib/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../lib/bootstrap/dist/css/open-iconic-bootstrap.min.css" />
   <!-- <link rel="stylesheet" href="lib/DataTables/datatables.min.css" /> -->
-  <link rel="stylesheet" href="styles/site.css" />
-  <link rel="stylesheet" href="styles/efa.css" />
-  <link rel="stylesheet" type="text/css" href="styles/topnav.css"/>
+  <link rel="stylesheet" href="../styles/site.css" />
+  <link rel="stylesheet" href="../styles/efa.css" />
+  <link rel="stylesheet" type="text/css" href="../styles/topnav.css"/>
 </head>
 
 <body>
   <div class="topnav">
-    <a href="index.php">Home</a>
-    <a class="active" href="catalogue.php">Catalogue</a>
-    <a href="contact.php">Contact</a>
-    <a href="technical.php">Technical</a>
-    <a class="active mode" href="contact.php" id="light_mode">Light Mode</a>
-    <a href="dark_contact.php" id="dark_mode" class="mode">Dark Mode</a>
+    <a href="../index.php">Home</a>
+    <a class="active" href="../catalogue.php">Catalogue</a>
+    <a href="../contact.php">Contact</a>
+    <a href="../technical.php">Technical</a>
+    <a class="active mode" href="../contact.php" id="light_mode">Light Mode</a>
+    <a href="../dark_contact.php" id="dark_mode" class="mode">Dark Mode</a>
   </div>
-  <?php include 'includes/vpn_warning_light.php';?>
+  <?php include '../includes/vpn_warning_light.php';?>
   <div class="mb-2 mt-2">
       <h2 class="title-color mb-3 text-center">ဖက်ဒရယ် တက္ကသိုလ်</h2>
       <h2 class="title-color mb-2 text-center">Virtual Federal University</h2>
