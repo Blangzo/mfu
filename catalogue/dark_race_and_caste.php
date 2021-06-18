90d1c9e0671dc984782231bc518ab1b97c62f94c
Wesley <wh395@cornell.edu>
2021-06-18 01:06:42 -0400

    updated race and caste catalogue

--- catalogue/dark_race_and_caste.php
+++ catalogue/dark_race_and_caste.php
@@ -48,6 +48,19 @@
               <p class="i2"><a href="https://ipfs.io/ipfs/QmaHXtUxpDNs1LCut5ZJiVbpDT52RciCHgid4EM1W34xdj?filename=Caste%20Matters%20by%20Suraj%20Yengde%20(z-lib.org).pdf">Caste Matters by Suraj Yengde</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/Qmc8VYzY4VdjZAKAtB2PwipGoFuWA3GiDmBfCeisUvWF7e?filename=loomba-race_and_the_possibilities.pdf">Loomba Race And The Possibilities</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmYCBnW2XDCGNsGDpE1tGk93DX4z5DNWS4c14RG3XyLm6c?filename=The%20Intimacies%20of%20Four%20Continents%20by%20Lisa%20Lowe%20(z-lib.org).pdf">The Intimacies of Four Continents by Lisa Lowe</a>
+          <h4 class="module">Module 2: Slavery, Indentured Labour and the Making of the Modern World</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmY7XLU5TJvoB5FRFAJ5r7SYXtB31kZpW61eBaF7gi1Qox?filename=Race%20and%20Caste%202%20-%20audio.m4a">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQk1gqdnSHRUbUedcCGi2k1qGmh3oiRckGK8i6foxQuuu?filename=Race%20and%20Caste%202%20-%20transcript.docx">English Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmWgJ3XBvD1j1XqjTcere3BUP2PzaWpr82BsXP5AB8eEDH?filename=Indian%20Migration%20and%20Empire%20A%20Colonial%20Genealogy%20of%20the%20Modern%20State%20by%20Radhika%20Mongia.pdf">Indian Migration and Empire A Colonial Genealogy of the Modern State by Radhika Mongia</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmdAKNKLViQ9ZXkLC9iNyohbeoFuXRKuMJ1piCBGsafQPt?filename=Capitalism%20and%20Slavery%20by%20Eric%20Williams.pdf">Capitalism and Slavery by Eric Williams</a></p>
+          <h4 class="module">Module 3: Annihilation of Caste</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmWpfVw3aWDibSXcoHKyt5BN44yCDqCFh7wKWzSdX2w2rf?filename=Lesson%203.mp3">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmTZwXqFmyYyycssFC1f1CbgWM8ZMGSAsB6mAM5KW86orJ?filename=Lesson%203%20-%20Annihilation%20of%20Caste.docx">English Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZD4bGFVx9Gf88MQ6bS6vNVYk6sahHShJawJB7BSKPh65?filename=annihilation%20of%20caste%20.pdf">Annihilation of Caste by B.R. Ambedkar</a></p>
       </div>
   </div>
   <form class="course">
