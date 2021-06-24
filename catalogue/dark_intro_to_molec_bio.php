25eadee1b88fcbfc1f900ad3fe0957f77b6af071
Wesley <wh395@cornell.edu>
2021-06-24 03:14:56 -0400

    uploaded intro to molec bio content

--- catalogue/dark_intro_to_molec_bio.php
+++ catalogue/dark_intro_to_molec_bio.php
@@ -20,12 +20,12 @@
 
 <body>
   <div class="topnav">
-    <a href="../index.php">Home</a>
-    <a class="active" href="../catalogue.php">Catalogue</a>
-    <a href="../contact.php">Contact</a>
-    <a href="../technical.php">Technical</a>
-    <a class="active mode" href="intro_to_molec_bio.php" id="light_mode">Light Mode</a>
-    <a href="dark_intro_to_molec_bio.php" id="dark_mode" class="mode">Dark Mode</a>
+    <a href="../dark_index.php">Home</a>
+    <a class="active" href="../dark_catalogue.php">Catalogue</a>
+    <a href="../dark_contact.php">Contact</a>
+    <a href="../dark_technical.php">Technical</a>
+    <a class="mode" href="intro_to_molec_bio.php" id="light_mode">Light Mode</a>
+    <a href="dark_intro_to_molec_bio.php" id="dark_mode" class="active mode">Dark Mode</a>
   </div>
 
   <div class="mb-2 mt-2">
@@ -36,8 +36,47 @@
 
   <div class="mb-4 mt-4">
       <div class="mb-2 mt-3">
-        <h3 class="course">SEAsia Cold War - Instructor name withheld</h3>
-
+        <h3 class="course">Intro to Molecular Biology - Instructor name withheld</h3>
+        <h5><a href="https://ipfs.io/ipfs/QmUUQG2qf5gD8CdRjkFJPEeQsr77ULvrQb27t5YrvCWGv6?filename=Guidelines%20for%20Intro%20to%20Molecular%20Bio.pdf">Guidelines for Intro to Molecular Biology</a></h5>
+        <h5>Introduction</h5>
+        <p><a href="https://ipfs.io/ipfs/QmZoMofgDD4ipt1nyM1ymUBDai2YcNrbUTjzV4AVpQA53j?filename=Intro%20to%20Molecular%20Biology_Burmese.pdf">Introduction PDF</a></p>
+        <p><a href="https://ipfs.io/ipfs/Qmf5DJYkT9U7WNLRL8b6uXdVmF4dR9qrfj17NDTTadWmV5?filename=Introduction%20to%20Molecular%20Biology_Burmese.m4a">Introduction audio</a></p>
+        <h5><a href="https://ipfs.io/ipfs/QmY56qEpC5CnvReMugVU7tHodPtCFRqis4gFdqAXsj7KVV?filename=Biology2e-WEB_ICOFkGu.pdf">Textbook</a></h5>
+        <p>The textbook is also available online <a href="https://openstax.org/details/books/biology-2e">here</a></p>
+        <table>
+          <tr>
+            <th>Lesson</th>
+            <th>Chapter</th>
+            <th>Audio</th>
+            <th>Slides</th>
+            <th>Transcript</th>
+            <th>Video</th>
+          </tr>
+          <tr>
+            <th>Lesson 1</th>
+            <th>Ch. 1</th>
+            <th><a href="https://ipfs.io/ipfs/QmZUTrU336dXvoDy5v36Uvsq4UzbASM9rjV33mCfeXvTcP?filename=Lesson1_1_The%20process%20of%20science.m4a">Lesson 1.1 The process of Science</a><br/><a href="https://ipfs.io/ipfs/QmP1TcKvV7eXPi4QuQgB8sztioS6HKBmw8h57tinyEfBSu?filename=Lesson%201_2_The%20process%20of%20science.m4a">Lesson 1.2 The process of Science</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmYB4JcgEjiHAE8qzKnssCvxg3C1xhe4hcrZYPrcnZMaqn?filename=Lesson%201_The%20process%20of%20science_Slides.pdf">Lesson 1 The process of Science Slides</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmdtCJiEetcezPmBmXsE3dWz4FnU31aJ2Znhx2f3UhHRTN?filename=Lesson%201_The%20process%20of%20science.pdf">Lesson 1 The process of Science Transcript</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmWw2FPzFpj7Sj1Hvom6AJiHN61YBv2rPkMuQwe9pPu51f?filename=Lesson1_1_The%20process%20of%20science.mp4">Lesson 1.1 The process of Science</a><br/><a href="https://ipfs.io/ipfs/QmSjN6Er6TeKUMbnJgmyzXo7yp3GrcvQCaJL16tCruVex4?filename=Lesson%201_2_The%20process%20of%20science.mp4">Lesson 1.2 The process of Science</a></th>
+          </tr>
+          <tr>
+            <th>Lesson 2</th>
+            <th>Ch. 2</th>
+            <th><a href="https://ipfs.io/ipfs/QmcLgfb8E65cUpdanPTQSk4UwPvUNvogQ5u64wrrfXsuTR?filename=Lesson%202_1_Chemistry%20of%20Life.m4a">Lesson 2.1 Chemistry of Life</a><br/><a href="https://ipfs.io/ipfs/QmZYfmdzCyPhoFvqLY5yWo3BoidUv1uAYdAJefXbvAL8KQ?filename=Lesson%202_2_Chemistry%20of%20Life.m4a">Lesson 2.2 Chemistry of Life</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmX9hgHraUWtvmySFkpFHqhRq6gE9j7TjmU1QhM4D8SEmQ?filename=Lesson%202_Chemistry%20of%20Life_Slides.pdf">Lesson 2 Chemistry of Life Slides</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmXhmGrM3ZW6bShBV2jAHSUzNBjA35vPZyXtm9Vn1S6mCR?filename=Lesson%202_Chemistry%20of%20Life.pdf">Lesson 2 Chemistry of Life Transcript</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmRQg1WxPNmfEjy75Wj93zBt5BQuWZ2VJUVwAwysuN18T1?filename=Lesson%202_1_Chemistry%20of%20Life.mp4">Lesson 2.1 Chemistry of Life</a><br/><a href="https://ipfs.io/ipfs/QmZkhceWMRuDCKMxFGVPAznUuvoKf7xQtQDExgsNhjiDQL?filename=Lesson%202_2_Chemistry%20of%20Life.mp4">Lesson 2.2 Chemistry of Life</a></th>
+          </tr>
+          <tr>
+            <th>Lesson 3</th>
+            <th>Ch. 3</th>
+            <th><a href="https://ipfs.io/ipfs/QmaGSjrRuDGe9rSsqT8bU5zzTDaAkLzKDWWgUZ6teWdDSx?filename=Lesson%203_1_Biomolecules.m4a">Lesson 3.1 Chemistry of Life</a><br/><a href="https://ipfs.io/ipfs/QmR7Jdddw9DwNY2UEawhnto5PVGFGmwWyTAvBwuUkq8ChD?filename=Lesson%203_2_Biomolecules.m4a">Lesson 3.2 Biomolecules</a><br/><a href="https://ipfs.io/ipfs/QmedKaNJDyqKqh9xaciWakNYmV6685WmLnRuBiVQizcCai?filename=Lesson%203_3_Biomolecules.m4a">Lesson 3.3 Biomolecules</a><br/><a href="https://ipfs.io/ipfs/QmcruVbQ6tj6saLJDUpQPAxKAwbYrn4ee7Pf3x7xTt8Doi?filename=Lesson%203_4_Biomolecules.m4a">Lesson 3.4 Biomolecules</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmbSww6qAy6QCRt7E3S4NZK3Cfz6j6LHAdgfZzjcgtec5E?filename=Lesson%203_Biomolecules_Slides.pdf">Lesson 3 Biomolecules Slides</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmeJk5BbiWfT89nNruDxfR7JLMcmysHF36mPLX1PuEdUam?filename=Lesson%203_Biomolecules.pdf">Lesson 3 Biomolecules Transcript</a></th>
+            <th><a href="https://ipfs.io/ipfs/QmTuvknj4ZPZvJwvmZDxne45UeeMft3Zr9339khNDoXBnK?filename=Lesson%203_1_Biomolecules.mp4">Lesson 3.1 Chemistry of Life</a><br/><a href="https://ipfs.io/ipfs/QmUXZFpHfEkFx27iPz7MVEV7TPiJ3QUsex7cpPi4nPvNfu?filename=Lesson%203_2_Biomolecules.mp4">Lesson 3.2 Biomolecules</a><br/><a href="https://ipfs.io/ipfs/Qmd8xZ2fZQ9jeciTFxVFD6KCZPQpjkrdUHSRx6dSVYgPNr?filename=Lesson%203_3_Biomolecules.mp4">Lesson 3.3 Biomolecules</a><br/><a href="https://ipfs.io/ipfs/QmXFvwbh5vi1cBrodqtCxr23rWKvUyhAihgCPACcg15aEx?filename=Lesson%203_4_Biomolecules.mp4">Lesson 3.4 Biomolecules</a></th>
+          </tr>
+        </table>
       </div>
   </div>
   <form class="course">
