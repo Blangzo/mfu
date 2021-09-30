8a087a9538e9104fb3a248fea229cce4f82884ab
Wesley <wh395@cornell.edu>
2021-09-29 20:45:42 -0400

    added burmese and jingphaw audio and transcripts for lesson 1&2

--- catalogue/lies_my_teacher_told_me.php
+++ catalogue/lies_my_teacher_told_me.php
@@ -42,13 +42,22 @@
         <h3 class="course">Lies My Teacher Told Me by Dr. Rose Metro, University of Missouri-Columbia</h3>
           <h4 class="module">Module 1</h4>
             <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmZ1SShNVDpCqy8wjUYgj4R1gezrXJV5JQkZBTvA6bLPz6?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZ1SShNVDpCqy8wjUYgj4R1gezrXJV5JQkZBTvA6bLPz6?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmdpnBfhAaFEXEMZpy9cX2qCz4URnKvfGSf4tEkZmgQb96?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Burmese Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPFv645bdJR3yxMR94NGf15hU2ijobwUqa1raj2XfCNKv?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Jingphaw Audio</a></p>
             <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmQyztcXGHACdJqQUxeXAMDEJapKYW5AvWneLGTqaVViS8?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQyztcXGHACdJqQUxeXAMDEJapKYW5AvWneLGTqaVViS8?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmaMgVzuJiftcfRXxBDDngJu997ERFwFNh1gdCTP5YhGVt?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmNW1Vn4Y9v7hzE5EDkCRtu3e8fqWuwm8kRorrgoYqjL2Y?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Jingphaw Transcript</a></p>
           <h4 class="module">Module 2</h4>
             <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmNa5Vs5L9FXtxBL4WyEJ3rND1yVFrcTB1cUfXakRnSw6J?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Audio</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmQHm9m4dM4MqWBgBSEoXyMQpXUBr7ZhtPj51WvGGAg7xX?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmNa5Vs5L9FXtxBL4WyEJ3rND1yVFrcTB1cUfXakRnSw6J?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmY7sfFR5kxqjBrG33D6xzUpXGL4eNqnoop4B946F5SgVH?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Burmse Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmcQV9LL6GmP874f3CgkdHwuXh279iipAB4xoNVy32iNkC?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQHm9m4dM4MqWBgBSEoXyMQpXUBr7ZhtPj51WvGGAg7xX?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQRqA2NTGTuiWT4Bzcuxtw8oN1XHMU3ZvnRMW4JGXGhSS?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPBLK4cTsRgusDAU76GZan4JzvryoySTsBrh9R9L1fmah?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Jingphaw Transcript</a></p>
       </div>
   </div>
   <form class="course">
