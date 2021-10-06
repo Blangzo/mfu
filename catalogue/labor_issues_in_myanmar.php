1431011c8aa945a7ecfa7bc903bcaa433d6e5d4e
vfu228 <91992118+vfu228@users.noreply.github.com>
2021-10-06 03:27:27 -0400

    Labour History PDFs without comments

--- catalogue/labor_issues_in_myanmar.php
+++ catalogue/labor_issues_in_myanmar.php
@@ -44,26 +44,26 @@
             <h5 class="i1">Lecture</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmeHaTAP5o8RhRRuF9sYvL1VhhzB1ZJofSC4scveKiqe2t?filename=Labor_Issues_in_Myanmar_Lecture1.m4a">Burmese Audio</a></p>
             <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmawSjpZXcLHqZwFQw7XhnSBParKRWVnsX4gptmGq9m9a2?filename=Labour_Issues_in_Myanmar_History_Lecture1.pdf">Burmese Transcript</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmagstFjwzBk5Mp7UA1fNfbSDrGXWYncsah8MV5Nm5Nqfi?filename=Labor_Issues_in_Myanmar_Lecture1.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/Qmaccs7wX8nYaxVueRaJfSMkBgmwJWNU8vRcWPFVbd9WAb?filename=Labour_Issues_in_Myanmar_History_Lecture1.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmXCw24eZkXmB6g84SyETy9xVJjo945VVtUuQ3qdysosLo?filename=Labor_Issues_in_Myanmar_Lecture1.pdf">English Transcript</a></p>
           <h4 class="module">Module 2</h4>
             <h5 class="i1">Lecture</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmXS2GhS2EGzXYe3C7YBp9foyx52WuL3DF1DNo8MZANj7W?filename=Labor_Issues_in_Myanmar_Lecture2.mp3">Burmese Audio</a></p>
             <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmYhtHCFu86sLfF8a8pfUERas4faskMjzbfTZDBt9bL78j?filename=Labor_Issues_in_Myanmar_Lecture2.docx">English Transcript</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmSNUvq5YJ4VFvmP2d3U1yxPe5zDwX7XLzkvprWLYe37Gv?filename=Labor_Issues_in_Myanmar_Lecture2.docx">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmTU9X8jtT3FL9t8wNGzppumQ6L5uaGmc6JWsXvi18xjUt?filename=Labor_Issues_in_Myanmar_Lecture2.docx">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPg2NZH4GvK1dyBn6ojdtqfH3zktNajABA38M21vrPwxV?filename=Labor_Issues_in_Myanmar_Lecture2.docx">Burmese Transcript</a></p>
           <h4 class="module">Module 3</h4>
             <h5 class="i1">Lecture</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmcWQ4VupXUXB3Rv2du8i7ubCU43XANFDRksFm6AzpzoQg?filename=Labor_Issues_in_Myanmar_Lecture3.m4a">Burmese Audio</a></p>
             <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmNX6GJZtCgVijuQwzE6fqHwmPwjSu8BHAvsLrMU59t5gm?filename=Labor_Issues_in_Myanmar_Lecture3.docx">English Transcript</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmYKgMsezw4b4CJv4pXgVZSzv7kAHaFkqJKdaQ9bccpFiu?filename=Labor_Issues_in_Myanmar_Lecture3.docx">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmUVjzoXPfARnEd5f7ui68d8mhHwhZ1q7EHuYevrGFVyyr?filename=Labor_Issues_in_Myanmar_Lecture3.docx">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmexhFQEyn6Xh78YVaZsyYhmDQbxKEySTegEF1WodQsBcb?filename=Labor_Issues_in_Myanmar_Lecture3.docx">Burmese Transcript</a></p>
           <h4 class="module">Module 4</h4>
             <h5 class="i1">Lecture</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmNpBpfj9Vqb47Kt2DK5ke2hqZnh6EJqAGhgHcnYY1cG36?filename=Labor_Issues_in_Myanmar_Lecture4.m4a">Burmese Audio</a></p>
             <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmR1MnyE5pbPstDKNnH72tbfmBky9sMLmzsg968hhqaiTq?filename=Labor_Issues_in_Myanmar_Lecture4.docx">English Transcript</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmXiW2kn9ZSuPeZqNYYzDymGT8JvrExFbZbd8QtvM5x9QR?filename=Labor_Issues_in_Myanmar_Lecture4.docx">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQKgezkhYaaVhpNX7PuMUJ1ftaHEbfyx1EGSVoGZb6fsW?filename=Labor_Issues_in_Myanmar_Lecture4.docx">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmXqx2LwZjAj2RVE3NvDiMNParTuerNqkESL2Gabn6zaGK?filename=Labor_Issues_in_Myanmar_Lecture4.docx">Burmese Transcript</a></p>
       </div>
   </div>
   <form class="course">
