b4302902ce3979b768ee187ad77486c91c0b237f
vfu228 <91992118+vfu228@users.noreply.github.com>
2021-10-06 04:12:17 -0400

    labour instructors edit

--- catalogue/labor_issues_in_myanmar.php
+++ catalogue/labor_issues_in_myanmar.php
@@ -40,7 +40,7 @@
   <div class="mb-4 mt-4">
       <div class="mb-2 mt-3">
         <h3 class="course">Labour Issues in Myanmar History</h3>
-        <h4 class="course">Ko Maung and Stephen Campbell</h4>
+        <h4 class="course">Taught by Ko Maung and Stephen Campbell</h4>
             <h5 class="i1">Course Abstract</h5>
           <p class="i2">This course will introduce students to various issues and theories in labour studies through a survey of Myanmar labour history since the colonial period. The aim is to provide students with conceptual tools to analyse labour and employment dynamics and trends in their own country and elsewhere. The course will familiarise students with key moments in Myanmar labour history, and with the importance of workers and labour in the country’s development. By highlighting ongoing theoretical debates about labour  and  employment,  the  course  will  prompt  students  to  draw  on  their  own  experience  and knowledge  to  make  sense  of  the  diversity  of  historical  and  contemporary  labour  arrangements  in Myanmar, and labour arrangements affecting Myanmar migrant workers abroad. </p>
           <h4 class="module">Module 1</h4>
