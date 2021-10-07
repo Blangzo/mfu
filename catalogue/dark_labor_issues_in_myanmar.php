bdbdeb971c50b97c2f9557a221ee270f623f0730
Wesley <wh395@cornell.edu>
2021-10-07 10:01:12 -0400

    added readings to labor issues in myanmar

--- catalogue/dark_labor_issues_in_myanmar.php
+++ catalogue/dark_labor_issues_in_myanmar.php
@@ -46,24 +46,44 @@
             <h5 class="i1">Transcript</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmawSjpZXcLHqZwFQw7XhnSBParKRWVnsX4gptmGq9m9a2?filename=Labour_Issues_in_Myanmar_History_Lecture1.pdf">Burmese transcript</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmagstFjwzBk5Mp7UA1fNfbSDrGXWYncsah8MV5Nm5Nqfi?filename=Labor_Issues_in_Myanmar_Lecture1.pdf">English Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2">No reading assignment</p>
+              <p class="i2">ဖတ်စာ မရှိပါ။</p>
           <h4 class="module">Module 2</h4>
             <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmXS2GhS2EGzXYe3C7YBp9foyx52WuL3DF1DNo8MZANj7W?filename=Labor_Issues_in_Myanmar_Lecture2.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs /QmXS2GhS2EGzXYe3C7YBp9foyx52WuL3DF1DNo8MZANj7W?filename=Labor_Issues_in_Myanmar_Lecture2.mp3">English Audio</a></p>
             <h5 class="i1">Transcript</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmYhtHCFu86sLfF8a8pfUERas4faskMjzbfTZDBt9bL78j?filename=Labor_Issues_in_Myanmar_Lecture2.docx">English Transcript</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmSNUvq5YJ4VFvmP2d3U1yxPe5zDwX7XLzkvprWLYe37Gv?filename=Labor_Issues_in_Myanmar_Lecture2.docx">Burmese Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2">Yenan, Thein Phay Myint, Myanmar A Lin Hnit Thịt, Mingalar Newspaper, April, 1938
+                <ol> Oilfield is where strikes first started, Yebaw Tun Oo</ol>
+                <ol> Strike War, Thakhin Pho Hla Gyi, 1938 </ol> </p>
+                <p class="i2">ရေနံ၊ သိန်းဖေမြင့်၊ မြန်မာ့အလင်းနှစ်သစ်၊ မင်္ဂလာသတင်းစာ၊ ဧပြီ၊ ၁၉၃၈။
+                  <ol> သပိတ်အစ ရေနံမြေက၊ ရဲဘော်ထွန်းဦး</ol>
+                  <ol> သပိတ်စစ်ပွဲ၊ သခင်ဘိုးလှကြီး၊ ၁၉၃၈။ </ol> </p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmU8h61errBewSnKmjTyTpstmUyDqCKaUNSa6WswEqSUfF?filename=YeBaw_Htun_Oo_Oil_Field_Strike.jpg">Download Link for Oilfield Image</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmUFsqHkp559sAVq2JEZMTJR4WfSbFFjHsXBnhJ3Z51mb4?filename=Strike_War_Thakhin_Pho_Hia_Gyi_1938.pdf">Download Link for Strike war</a></p>
           <h4 class="module">Module 3</h4>
             <h5 class="i1">Lecture</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmcWQ4VupXUXB3Rv2du8i7ubCU43XANFDRksFm6AzpzoQg?filename=Labor_Issues_in_Myanmar_Lecture3.m4a">English Audio</a></p>
             <h5 class="i1">Transcript</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmNX6GJZtCgVijuQwzE6fqHwmPwjSu8BHAvsLrMU59t5gm?filename=Labor_Issues_in_Myanmar_Lecture3.docx">English Transcript</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmYKgMsezw4b4CJv4pXgVZSzv7kAHaFkqJKdaQ9bccpFiu?filename=Labor_Issues_in_Myanmar_Lecture3.docx">Burmese Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2">Strike War, Thakhin Pho Hla Gyi, 1938</p>
+              <p class="i2">သပိတ်စစ်ပွဲ၊ သခင်ဘိုးလှကြီး၊ ၁၉၃၈။</p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmUFsqHkp559sAVq2JEZMTJR4WfSbFFjHsXBnhJ3Z51mb4?filename=Strike_War_Thakhin_Pho_Hia_Gyi_1938.pdf">Download Link</a></p>
           <h4 class="module">Module 4</h4>
             <h5 class="i1">Lecture</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmNpBpfj9Vqb47Kt2DK5ke2hqZnh6EJqAGhgHcnYY1cG36?filename=Labor_Issues_in_Myanmar_Lecture4.m4a">English Audio</a></p>
             <h5 class="i1">Transcript</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmR1MnyE5pbPstDKNnH72tbfmBky9sMLmzsg968hhqaiTq?filename=Labor_Issues_in_Myanmar_Lecture4.docx">English Transcript</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmXiW2kn9ZSuPeZqNYYzDymGT8JvrExFbZbd8QtvM5x9QR?filename=Labor_Issues_in_Myanmar_Lecture4.docx">Burmese Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2">An unlisted occupation, Ludu Daw Amar, Atway Amyin Magazine, 1987</p>
+              <p class="i2">စာရင်းမပေါက်တဲ့လုပ်အားခ၊ လူထုဒေါ်အမာ၊ အတွေးအမြင်၊ ၁၉၈၇။</p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmfVWh1Q9YAYo786tVyssQZKYAdztRJXpEvw6qQA5B36qd?filename=Ludu_Daw_Amar_Atway_Amyin_Magazine_1987.pdf">Download Link</a></p>
       </div>
   </div>
   <form class="course">
