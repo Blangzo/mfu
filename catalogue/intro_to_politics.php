50efab61512b5565921b15efc2c662b7a1e506c8
Wesley <wh395@cornell.edu>
2021-10-24 19:18:53 -0400

    added audio to politics course, added note to catalogue

--- catalogue/intro_to_politics.php
+++ catalogue/intro_to_politics.php
@@ -47,13 +47,15 @@
         <h5 class="i1"><a href="https://ipfs.io/ipfs/QmZGE1d1D6ocud8Gw1bUPHLRw4H1AxjUcRnU649Qx3Pnxs?filename=Questions%20and%20Answers%20Units%201-4.pdf">Questions and Answers Units 1-4</a></h5>
           <h4 class="module">Unit 1: What is Politics?</h4>
             <h5 class="i1">Lectures</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmQm2qbuEehkqctpCzDCMJv1H19ft8hmhZ44SFnB9qiJHw?filename=What%20is%20Politics.mp3">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQm2qbuEehkqctpCzDCMJv1H19ft8hmhZ44SFnB9qiJHw?filename=What%20is%20Politics.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPWqnSuXGKSWAHje3c4SMcdBMXqAWfzR37APHXbgxd5wt?filename=What%20is%20Politics.mp3">Burmese Audio</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmQ8cYxdvidG178drLw2LRs6pHAzuATSn9RfppLZnEgGy5?filename=What%20is%20Politics.pdf">English Transcript</a></p>
             <h5 class="i1">Readings</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmeZdJyTbHRsBcff25J1hQZ8Nqd59NFVcSKgc8oTgdokhx?filename=Thawnghmung%202011%20Abstract%20to%20her%20Article.pdf">Thawnghmung 2011 Abstract to her Article</a>
           <h4 class="module">Unit 2: Studying Politics</h4>
             <h5 class="i1">Lectures</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmZJFJtMmgnvywrhztWHnCupKsTjsc3T9R3qFGv6boi9Nm?filename=Studying%20Politics.mp3">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZJFJtMmgnvywrhztWHnCupKsTjsc3T9R3qFGv6boi9Nm?filename=Studying%20Politics.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmaPP4yMdYXvPWrxp9QnNns1PThdraAgTk6vGUcKzMwca8?filename=What%20is%20Politics.mp3">Burmese Audio</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmPXZjWQCRfPEexPjSen4cYGZa1CoZJMUf9a24p4Y8Nxss?filename=Studying%20Politics.pdf">English Transcript</a></p>
             <h5 class="i1">Readings</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmWLvza654CiQwvfX3W7TPtY57y1cV2hGcR3bHAMahS215?filename=Approaches%20ands%20Methodss%20tos%20Studyings%20Politics.pdf">Approaches and Methods to Studying Politics</a></p>
@@ -61,14 +63,16 @@
               <p class="i2"><a href="https://ipfs.io/ipfs/QmQMpt22BnCEKwpPGhT45Ao57rkvq1CYMuEdN5m4Sva5JD?filename=Research%20Skills.pdf">Research Skills</a></p>
           <h4 class="module">Unit 3: Ideas in Politics</h4>
             <h5 class="i1">Lectures</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmU9Y9fkbzVnyi4wrQnj5TQpyhtaNY2EW4Qzc8Te6vjHei?filename=Ideas%20in%20Politics.mp3">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmU9Y9fkbzVnyi4wrQnj5TQpyhtaNY2EW4Qzc8Te6vjHei?filename=Ideas%20in%20Politics.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmRE3ga2XYxdnkLqEgP1vo4kVFB1xHACRpQL2BAuqsdrHM?filename=What%20is%20Politics.mp3">Burmese Audio</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmSkhKjwvdHJr4XLyJQJkdXcRnpF1c5VTCWVdUbfnnumfU?filename=Ideas%20in%20Politics.pdf">English Transcript</a></p>
             <h5 class="i1">Readings</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmVrT2mbuA1BxHbhfTJAd6BmcYtZf5BxFNzJptXCAmbtSf?filename=Freedom%20and%20Equality%20adapted%20from%20ONeil.pdf">Freedom and Equality adapted from ONeil</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmNVjz3LTARXj2WhYpfdqzGQyfEXREuvfCDstazc6ZgynG?filename=Thawnghmung%202003%20Rural%20perceptions%20of%20state%20legitimacy%20in%20Burma%20Myanmar.pdf">Thawnghmung 2003 Rural perceptions of state legitimacy in Burma Myanmar</a></p>
           <h4 class="module">Unit 4: The State</h4>
             <h5 class="i1">Lectures</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmZqzMRXbyS8m9V8qYgys86bzoptqhtpvv2G4oNLu8LZKc?filename=The%20State%20.mp3">Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZqzMRXbyS8m9V8qYgys86bzoptqhtpvv2G4oNLu8LZKc?filename=The%20State%20.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmVeq1iwqD8U9raJiaXXebvZvZfZja2c98yvSS9zXBQnpE?filename=What%20is%20Politics.mp3">Burmese Audio</a></p>
               <p class="i2"><a href="https://ipfs.io/ipfs/Qmd4DLhVR9tVZXECk5rqz7inqzuMe1Rg3ZcYuRzow4ivfZ?filename=The%20State%20.docx">English Transcript</a></p>
             <h5 class="i1">Readings</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmNMdwxF5BaGsX9EcPJUSyosNyohT9v1zojtd6sgnyRzHg?filename=Is%20the%20State%20a%20force%20for%20good%20Heywood.pdf">Is the State a force for good [Heywood]</a></p>
