4cf3b9407a79d68c4b6f35dd9c33f491602b5d6f
Wesley <wh395@cornell.edu>
2021-11-11 23:10:16 -0500

    added units 3-6 recordings & transcripts, eng, bur&jing to lies

--- catalogue/dark_lies_my_teacher_told_me.php
+++ catalogue/dark_lies_my_teacher_told_me.php
@@ -42,26 +42,62 @@
         <h3 class="course">Lies My Teacher Told Me by Dr. Rose Metro, University of Missouri-Columbia</h3>
         <h5 class="i1">Course Abstract</h5>
         <p class="i2">This course will cover 5 “myths” about Burma’s history: “master narratives” that have been repeated in school textbooks and state media for so long that they may subtly influence even people who are skeptical of government propaganda. The first episode will cover how history curricula have been used as political tools in Burma and worldwide. The other 5 episodes use primary source documents to deconstruct these 5 myths by examining how they came about and who they benefit: 1. All the ethnic groups were happily united under the rule of the Burmans before the British colonized the region. 2. There are 8 national races and 135 ethnic groups indigenous to Myanmar. 3. All the national races agreed to unify in the Panglong Agreement. 4. Myanmar is a Buddhist country. 5. The 1962 military coup was necessary to prevent the country from disintegrating.</p>
-          <h4 class="module">Module 1</h4>
+          <h4 class="module">Module 1 History Curriculum as a Political Tool</h4>
             <h5 class="i1">Lecture</h5>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmZ1SShNVDpCqy8wjUYgj4R1gezrXJV5JQkZBTvA6bLPz6?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">English Audio</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmdpnBfhAaFEXEMZpy9cX2qCz4URnKvfGSf4tEkZmgQb96?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Burmese Audio</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmPFv645bdJR3yxMR94NGf15hU2ijobwUqa1raj2XfCNKv?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Jingphaw Audio</a></p>
-          <h5 class="i1">Transcript</h5>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmQyztcXGHACdJqQUxeXAMDEJapKYW5AvWneLGTqaVViS8?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">English Transcript</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmaMgVzuJiftcfRXxBDDngJu997ERFwFNh1gdCTP5YhGVt?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Burmese Transcript</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmNW1Vn4Y9v7hzE5EDkCRtu3e8fqWuwm8kRorrgoYqjL2Y?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Jingphaw Transcript</a></p>
-        <h4 class="module">Module 2</h4>
-          <h5 class="i1">Lecture</h5>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmNa5Vs5L9FXtxBL4WyEJ3rND1yVFrcTB1cUfXakRnSw6J?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">English Audio</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmY7sfFR5kxqjBrG33D6xzUpXGL4eNqnoop4B946F5SgVH?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Burmse Audio</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmcQV9LL6GmP874f3CgkdHwuXh279iipAB4xoNVy32iNkC?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Jingphaw Audio</a></p>
-          <h5 class="i1">Transcript</h5>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmQHm9m4dM4MqWBgBSEoXyMQpXUBr7ZhtPj51WvGGAg7xX?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">English Transcript</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmQRqA2NTGTuiWT4Bzcuxtw8oN1XHMU3ZvnRMW4JGXGhSS?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Burmese Transcript</a></p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmPBLK4cTsRgusDAU76GZan4JzvryoySTsBrh9R9L1fmah?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Jingphaw Transcript</a></p>
-    </div>
-</div>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZ1SShNVDpCqy8wjUYgj4R1gezrXJV5JQkZBTvA6bLPz6?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmdpnBfhAaFEXEMZpy9cX2qCz4URnKvfGSf4tEkZmgQb96?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Burmese Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPFv645bdJR3yxMR94NGf15hU2ijobwUqa1raj2XfCNKv?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQyztcXGHACdJqQUxeXAMDEJapKYW5AvWneLGTqaVViS8?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmaMgVzuJiftcfRXxBDDngJu997ERFwFNh1gdCTP5YhGVt?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmNW1Vn4Y9v7hzE5EDkCRtu3e8fqWuwm8kRorrgoYqjL2Y?filename=Lies_My_Teacher_Told_Me_EP1_History_Curriculum_as_a_Political_Tool.pdf">Jingphaw Transcript</a></p>
+          <h4 class="module">Module 2 Myth of a Happily United Precolonial Burma</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmNa5Vs5L9FXtxBL4WyEJ3rND1yVFrcTB1cUfXakRnSw6J?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmY7sfFR5kxqjBrG33D6xzUpXGL4eNqnoop4B946F5SgVH?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Burmse Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmcQV9LL6GmP874f3CgkdHwuXh279iipAB4xoNVy32iNkC?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQHm9m4dM4MqWBgBSEoXyMQpXUBr7ZhtPj51WvGGAg7xX?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQRqA2NTGTuiWT4Bzcuxtw8oN1XHMU3ZvnRMW4JGXGhSS?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPBLK4cTsRgusDAU76GZan4JzvryoySTsBrh9R9L1fmah?filename=Lies_My_Teacher_Told_Me_EP2_Myth_of_a_Happily_United_Precolonial_Burma.pdf">Jingphaw Transcript</a></p>
+          <h4 class="module">Module 3 The Myth of 8 National Races and 135 Ethnic Groups</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmUqtTgewY7DFkCkjveohfPy4Y5X3bB8NbV41jn7WsqgZL?filename=Lies_My_Teacher_Told_Me_EP3_Myth_of_a_National_Races_and_135_Ethnic_Groups.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmVP9o3rAEdAf57RvpfPeX8WT8guQQG5bx5UUXobhbQMxp?filename=Lies_My_Teacher_Told_Me_EP3_Myth_of_a_National_Races_and_135_Ethnic_Groups.mp3">Burmse Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZRP1dvSRRkAnn6a8AoXpxrdYEiypZYpwiV7hsQr97HAy?filename=Lies_My_Teacher_Told_Me_EP3_Myth_of_a_National_Races_and_135_Ethnic_Groups.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmP7rNwTCf6Y1gMCnrtfyV8dedwucF5PTVCsDm5vTqHWW6?filename=Lies_My_Teacher_Told_Me_EP3_Myth_of_a_National_Races_and_135_Ethnic_Groups.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmSDsfiTqxiiJNmYdCPW4wSfdQC19zrC9CKv6t5sbJMVH2?filename=Lies_My_Teacher_Told_Me_EP3_Myth_of_a_National_Races_and_135_Ethnic_Groups.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmXH6FDyUZ2cBhDnvH3ioJjm3FpmhtVwMLQz1zLaMWPoFy?filename=Lies_My_Teacher_Told_Me_EP3_Myth_of_a_National_Races_and_135_Ethnic_Groups.pdf">Jingphaw Transcript</a></p>
+          <h4 class="module">Module 4 The Myth of Panglong</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmaJtAAw7iCZMK6exAhfsCvgwf7mQxhZ337VD6vvTmycaD?filename=Lies_My_Teacher_Told_Me_EP4_Myth_of_Panglong.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmR1G71kvNWznzNMFUoXPy1ZZdnHgG1KHeMFJ8t6BAuR6J?filename=Lies_My_Teacher_Told_Me_EP4_Myth_of_Panglong.mp3">Burmse Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQ1ZoiCmkEP2STDvCjFt2tnNVdATk1FB9UpdCaTH2m5b8?filename=Lies_My_Teacher_Told_Me_EP4_Myth_of_Panglong.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZD4ghdWYFKUYhJPYK9z3TmhescWnCJxPzBVAPbDkQnuV?filename=Lies_My_Teacher_Told_Me_EP4_Myth_of_Panglonga.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmejBHCvtkufkv2PzfgpMwEqGepkkGLr5f78gEPQ9rA1YD?filename=Lies_My_Teacher_Told_Me_EP4_Myth_of_Panglong.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmPBLK4cTsRgusDAU76GZan4JzvryoySTsBrh9R9L1fmah?filename=Lies_My_Teacher_Told_Me_EP4_Myth_of_Panglong.pdf">Jingphaw Transcript</a></p>
+          <h4 class="module">Module 5 The Myth of Burma as a Buddhist Country</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmdHSuHcNqgLuRe7puL4Eqyz8gc49Don1Unv9FP6DfCTUw?filename=Lies_My_Teacher_Told_Me_EP5_Myth_of_Burma_as_a_Buddhist_Country.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmePYmZ9BxuduMmacHxuh5k1qKcwoM6J2MYtibqbxJCnWz?filename=Lies_My_Teacher_Told_Me_EP5_Myth_of_Burma_as_a_Buddhist_Country.mp3">Burmse Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQLq3g77LaWwjxUhdpY8h6S6Q4UaPnz9Teg1w7Lv1Abev?filename=Lies_My_Teacher_Told_Me_EP5_Myth_of_Burma_as_a_Buddhist_Country.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmayyuAf7XtX5Gw8X8av89s9ZW4WXqdafkZniCgmSgD99R?filename=Lies_My_Teacher_Told_Me_EP5_Myth_of_Burma_as_a_Buddhist_Country.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZMwzEGS9TXFMM4gGg1tx4HM4nuL3f1W3Q14Xf8CC7LHm?filename=Lies_My_Teacher_Told_Me_EP5_Myth_of_Burma_as_a_Buddhist_Country.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmfJajLxhry8u99QLYip6HSLft335Jt5jEsq2bSo2BSqtU?filename=Lies_My_Teacher_Told_Me_EP5_Myth_of_Burma_as_a_Buddhist_Country.pdf">Jingphaw Transcript</a></p>
+          <h4 class="module">Module 6 The Myth of the Disintegration of Burma</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmRdHwTZZVhtDcC9MmtYteYVWL1pZ3u5AdbXZxZAmcr4yk?filename=Lies_My_Teacher_Told_Me_EP6_Myth_of_the_Disintegration_of_Burma.m4a">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/Qmech5XECg8dg3yaFZ8ybohRDgVcKGKyScvvJnMQM3KeZ5?filename=Lies_My_Teacher_Told_Me_EP6_Myth_of_the_Disintegration_of_Burma.mp3">Burmse Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmShBtyRZDWUzMZETbUG5g92VeDyqKhoZq5hGMLxwpEUvT?filename=Lies_My_Teacher_Told_Me_EP6_Myth_of_the_Disintegration_of_Burma.m4a">Jingphaw Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmWM9sPVjZog4iebZeXsUBPpcmr4N71d5y5ssVdPyPMy3k?filename=Lies_My_Teacher_Told_Me_EP6_Myth_of_the_Disintegration_of_Burma.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmVWLNcG6vNeFfs4MnQ6xxpUwaLzNQnagrWMHeaZjS7vwY?filename=Lies_My_Teacher_Told_Me_EP6_Myth_of_the_Disintegration_of_Burma.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmP6Pd5VzKisdFT1XNoXyZug4JmZx58AYAykdhAvrVmCKa?filename=Lies_My_Teacher_Told_Me_EP6_Myth_of_the_Disintegration_of_Burma.pdf">Jingphaw Transcript</a></p>
+      </div>
+  </div>
   <form class="course">
     <button onclick="location.href='../catalogue.php'" type="button">Back to catalogue</button>
   </form>
