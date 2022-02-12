ffa3576d35ee9e0de92632229f3d1d9f30d2d25b
Wesley <wh395@cornell.edu>
2022-02-11 23:32:35 -0500

    module 4 part 1 citizenship and statelessness

--- catalogue/dark_citizenship_and_statelessness.php
+++ catalogue/dark_citizenship_and_statelessness.php
@@ -73,12 +73,20 @@
               <p class="i2"><a href="https://ipfs.io/ipfs/QmSHbTFRH6uNQmgnQTZK2Pjx3cSXZqT5CTA1BC4esqGZSw?filename=lesson2_introdction_citizenship_and_statelessness_exercise_en.pdf">Lesson 2 Exercise</a></p>
           <h4 class="module">Module 3</h4>
             <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmQGqd7HFCWnwoMvgiWtpAKp97FtdQBLqfZMoM4TqqCwPp?filename=Lesson1_audio2_childhood_statelessness.mp3">English Audio, Part 1</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmT98zCwk87HvHCXQGLw6T53zn4v5qqXvnxWrrkebR3YdN?filename=Lesson3_audio1_childhood_statelessness.mp3">English Audio, Part 3</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQGqd7HFCWnwoMvgiWtpAKp97FtdQBLqfZMoM4TqqCwPp?filename=Lesson3_audio1_childhood_statelessness.mp3">English Audio, Part 1</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmT98zCwk87HvHCXQGLw6T53zn4v5qqXvnxWrrkebR3YdN?filename=Lesson3_audio3_childhood_statelessness.mp3">English Audio, Part 3</a></p>
             <h5 class="i1">Transcript</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmUTV7JwMiZZrGSrWBz2kn1qtEXscc78bTrxAKccRzWGWo?filename=Lesson3_transcript_childhood_statelessness.pdf">English Transcript (Part 1)</a></p>
             <h5 class="i1">Exercise</h5>
               <p class="i2"><a href="https://ipfs.io/ipfs/QmVrpWt993WRBMaWekqTVocWorF1G9zNiAsgFAuzwPfCp9?filename=lesson3_childhood_statelessness_with_logos_exercise.docx">Lesson 3 Exercise</a></p>
+          <h4 class="module">Module 4</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmXhWYyLBgCxn3nwjf5md6YgUxVCnoLbhFSYGLELgVK2oa?filename=Lesson1_audio2_childhood_statelessness.mp3">English Audio, Part 1</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmP7N6Wzgg6wUjgSW5qLuCASHUxE18XN5mNsmjKuE7NtYj?filename=Lesson3_transcript_childhood_statelessness.pdf">English Transcript (Part 1)</a></p>
+            <h5 class="i1">Reading</h5>
+              <img src="includes/lesson_4_part_1_citizenship_stripping_naviating_with_a_faulty_map.jpg" alt="stripping_naviating_with_a_faulty_map">
+              <p class="i2"><a href="https://files.institutesi.org/Access_to_Citizenship_in_Myanmar_Report.pdf">Download Report here</a></p>
       </div>
   </div>
   <form class="course">
