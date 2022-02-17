6140fc0fcff4b4a3a7c011bbd2648ac923af11c7
Wesley <wh395@cornell.edu>
2022-02-17 18:38:21 -0500

    added learning by playing course

---
+++ catalogue/learning_by_playing.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="my">
+
+<head>
+  <title>ဖက်ဒရယ် တက္ကသိုလ်</title>
+  <link rel="icon" sizes="16x16 32x32 64x64" href="vfulogoonly.ico"/>
+  <meta name="VFU" content="VFU" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php include 'includes/google_analytics_tag.php';?>
+  <!--<link rel="stylesheet" type="text/css" href="styles/v1.css"/>-->
+  <link rel="stylesheet" type="text/css" href="../styles/v2.css"/>
+  <link rel="stylesheet" href="../lib/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../lib/bootstrap/dist/css/open-iconic-bootstrap.min.css" />
+  <!-- <link rel="stylesheet" href="lib/DataTables/datatables.min.css" /> -->
+  <link rel="stylesheet" href="../styles/site.css" />
+  <link rel="stylesheet" href="../styles/efa.css" />
+  <link rel="stylesheet" type="text/css" href="../styles/topnav.css"/>
+  <link rel="stylesheet" type="text/css" href="../styles/catalogue.css"/>
+</head>
+
+<body>
+  <?php include 'includes/google_tag_manager_body.php';?>
+  <div class="topnav">
+    <a href="../index.php">Home</a>
+    <a class="active" href="../catalogue.php">Catalogue</a>
+    <a href="../contact.php">Contact</a>
+    <a href="../technical.php">Technical</a>
+    <a class="active mode" href="learning_by_playing.php" id="light_mode">Light Mode</a>
+    <a href="dark_learning_by_playing.php" id="dark_mode" class="mode">Dark Mode</a>
+  </div>
+
+  <div class="mb-2 mt-2">
+      <h2 class="title-color mb-3 text-center">ဖက်ဒရယ် တက္ကသိုလ်</h2>
+      <h2 class="title-color mb-2 text-center">Virtual Federal University</h2>
+  </div>
+  <div class="cat_body">
+  <hr />
+
+  <div class="mb-4 mt-4">
+      <div class="mb-2 mt-3">
+        <h3 class="course">Learning by Playing</h3>
+          <h5 class="i1">Course Guide for 0-12 months (modules 1-8)</h5>
+            <p>12-36 months not yet ready</p>
+            <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Learning_by_playing_0-12.pdf">Guidelines for Learning by Playing course</a></p>
+          <h4 class="module">Module 1</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_1/LbyP_L1.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_1/Transcript_L1.pdf">Transcript</a></p>
+          <h4 class="module">Module 2</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_2/LbyP_L2.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_2/Transcript_L2.pdf">Transcript</a></p>
+          <h4 class="module">Module 3</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_3/LbyP_L3.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_3/Transcript_L3.pdf">Transcript</a></p>
+          <h4 class="module">Module 4</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_4/LbyP_L4.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_4/Transcript_L4.pdf">Transcript</a></p>
+
+          <h4 class="module">Module 5</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_5/LbyP_L5.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_5/Transcript_L5.pdf">Transcript</a></p>
+
+          <h4 class="module">Module 6</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_6/LbyP_L6.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_6/Transcript_L6.pdf">Transcript</a></p>
+
+          <h4 class="module">Module 7</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_7/LbyP_L7.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_7/Transcript_L7.pdf">Transcript</a></p>
+
+          <h4 class="module">Module 8</h4>
+            <h5 class="i1">Lecture</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_8/LbyP_L8.mp4">Audio</a></p>
+            <h5 class="i1">Transcript</h5>
+              <p class="i2"><a href="https://gateway.pinata.cloud/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_8/Transcript_L8.pdf">Transcript</a></p>
+      </div>
+  </div>
+  <form class="course">
+    <button onclick="location.href='../catalogue.php'" type="button">Back to catalogue</button>
+  </form>
+</div>
+</body>
+</html>
