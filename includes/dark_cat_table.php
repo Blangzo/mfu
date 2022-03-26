f9e283b523a0ae17e76db9e14516eac0531c173c
Wesley Ho <wh395@cornell.edu>
2022-03-26 03:08:36 -0400

    Update dark_cat_table.php
    updated learning by playing name on dark catalogue

--- includes/dark_cat_table.php
+++ includes/dark_cat_table.php
@@ -47,7 +47,9 @@
 				<td>Name Withheld</td>
 			</tr>
 			<tr>
-				<td><a href="catalogue/dark_learning_by_playing.php">Learning by Playing</a></td>
+				<td><p><a href="catalogue/learning_by_playing.php">Learning by Playing</a></p>
+				    <p><a href="catalogue/learning_by_playing.php">ကစားရင်းဖွံ့ဖြိုး၊ ဖွံ့ဖြိုးရင်းကစား</a></p>
+				</td>
 				<td>ကစားရင်းဖွံ့ဖြိုး</td>
 			</tr>
    </tbody>
