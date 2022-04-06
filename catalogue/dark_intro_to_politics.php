60f29fe9c5c857889708405680a0b785bbcf8564
ho_we <ho_wesley@yahoo.com>
2022-04-05 20:39:26 -0400

    added politics units 9-12 burmese pdf and mp3

--- catalogue/dark_intro_to_politics.php
+++ catalogue/dark_intro_to_politics.php
@@ -121,34 +121,42 @@
                 <p class="i2"><a href="https://ipfs.io/ipfs/QmVwxS1SwXw71Pz52PZagvEeAWp3euqutRf3cJxD87k8gw?filename=Federalism_En.pdf">English Transcript</a></p>
               <h5 class="i1">Readings</h5>
                 <p class="i2"><a href="https://ipfs.io/ipfs/QmYr8vbW4EgAowxenRshY3DrQxr38554aPR6ZEC4QapTa9?filename=Hiestories%20of%20Burma%20Source%20book.pdf">Sai Wansai Commentary on the Shan federal proposal</a></p>
-            <h4 class="module">Unit 9: Elections and Political Parties</h4>
-              <h5 class="i1">Lectures</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmTK59bXdaLpRzKxqg63mCHNoGkex9yQWfJEGvAYsPXqss?filename=Elections-Political-Parties_English.mp3">English Audio</a></p>
-              <h5 class="i1">Transcripts</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmR6iVkJamYFPdxN3fV9WU3ejx4CtQyBQ912UskN6yYKoW?filename=Elections-Political-Parties_English.pdf">English Transcript</a></p>
-              <h5 class="i1">Readings</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmREVG3WpfmaMYXrRHmYuweKTwXef2F6Rx3sQWD3K28hN2?filename=Nu-Tsen-Mun-2020.pdf">Nu Tsen Mun 2020, Electoral systems at a crossroads</a></p>
-            <h4 class="module">Unit 10: Media and Politics</h4>
-              <h5 class="i1">Lectures</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmR7hAMSUMndLuZaNe7nDQgN2VjonUbpXmcc479mLhu227?filename=Media-Politics_English.mp3">English Audio</a></p>
-              <h5 class="i1">Transcripts</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmRLFHHZsiDQpXAWjTob2cobZs2F6YwujA8fkAPgpPKFSq?filename=Media-Politics_English.pdf">English Transcript</a></p>
-              <h5 class="i1">Readings</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmW3ACtEQNV7qQFDonhMbQffcJN3QFHJcaRU3BE4hcxDet?filename=Kemp-2017.pdf">Kemp 2017, Politics and the Media Chapter 1</a></p>
-            <h4 class="module">Unit 11: Civil Society</h4>
-              <h5 class="i1">Lectures</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmWnJ4feePgwoAXaoehmksDACpkwSoYc8363MviYvdjDdg?filename=Civil-Society_English.mp3">English Audio</a></p>
-              <h5 class="i1">Transcripts</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmP9NtqLtBQ8qeuVDnkqgUVWbSN4hX5qQyzQc45yQpsPYy?filename=Civil-Society_English.pdf">English Transcript</a></p>
-              <h5 class="i1">Readings</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmeRuYxqZjBSwVCBwuvGuUfBhaer7JCd6hjDh92swQ8SbW?filename=Petrie-South-2014.pdf">Petrie and South 2014, Development of civil soceity in Myanmar</a></p>
-            <h4 class="module">Unit 12: Global Politics</h4>
-              <h5 class="i1">Lectures</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmbRJAygxav8y3fSQyEBbY3vKCh7pqGX9uXfmKikAU85in?filename=Global-politics_English.mp3">English Audio</a></p>
-              <h5 class="i1">Transcripts</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmbCYoZUe5SPdKTF8x1uyAAMXfKDXaX3ZwyerbANyfE68P?filename=Global-politics_English.pdf">English Transcript</a></p>
-              <h5 class="i1">Readings</h5>
-                <p class="i2"><a href="https://ipfs.io/ipfs/QmSWATnMm8pvJmtpUY1b5sVHVeB3MsL3kz7M6AB7Nri5cJ?filename=Heywood-2011.pdf">Heywood 2011, Global Politics Chapter 1</a></p>
+          <h4 class="module">Unit 9: Elections and Political Parties</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmTK59bXdaLpRzKxqg63mCHNoGkex9yQWfJEGvAYsPXqss?filename=Elections-Political-Parties_English.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZcMPs5JPSPV9W1Yt6cCyhKwW8jt1wAnZf3hQRhp8wdV9?filename=Elections-Political-Parties_Burmese.mp3">Burmese Audio</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmR6iVkJamYFPdxN3fV9WU3ejx4CtQyBQ912UskN6yYKoW?filename=Elections-Political-Parties_English.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmTpY5PZmABVWAojd4YkFqFHs87xivrMrJx85jQUDxQFCm?filename=Elections-Political-Parties_Burmese.pdf">Burmese Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmREVG3WpfmaMYXrRHmYuweKTwXef2F6Rx3sQWD3K28hN2?filename=Nu-Tsen-Mun-2020.pdf">Nu Tsen Mun 2020, Electoral systems at a crossroads</a></p>
+          <h4 class="module">Unit 10: Media and Politics</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmR7hAMSUMndLuZaNe7nDQgN2VjonUbpXmcc479mLhu227?filename=Media-Politics_English.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmXz9eoqnYmTsqMLZFAEs9SdbYDR8aQQJPPNwEJtvpLELa?filename=Media-Politics_Burmese.mp3">Burmese Audio</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmRLFHHZsiDQpXAWjTob2cobZs2F6YwujA8fkAPgpPKFSq?filename=Media-Politics_English.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmeGzqjGgfkcgTC9DxGkW61tydJSuSMgohAD96pDM5dXQ9?filename=Media-Politics_Burmese.pdf">Burmese Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmW3ACtEQNV7qQFDonhMbQffcJN3QFHJcaRU3BE4hcxDet?filename=Kemp-2017.pdf">Kemp 2017, Politics and the Media Chapter 1</a></p>
+          <h4 class="module">Unit 11: Civil Society</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmWnJ4feePgwoAXaoehmksDACpkwSoYc8363MviYvdjDdg?filename=Civil-Society_English.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmZCrrsMko9tK1XZTsVu15cpdeJ97UA8abE3n8Wrk61kC8?filename=Civil-Society_Burmese.mp3">Burmese Audio</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/Qma1XwgcUby1X6Bib2P6DmeTUAAPvyu43k3omznEhkAN64?filename=Civil-Society_Burmese.pdf">Burmese Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmP9NtqLtBQ8qeuVDnkqgUVWbSN4hX5qQyzQc45yQpsPYy?filename=Civil-Society_English.pdf">English Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmeRuYxqZjBSwVCBwuvGuUfBhaer7JCd6hjDh92swQ8SbW?filename=Petrie-South-2014.pdf">Petrie and South 2014, Development of civil soceity in Myanmar</a></p>
+          <h4 class="module">Unit 12: Global Politics</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmbRJAygxav8y3fSQyEBbY3vKCh7pqGX9uXfmKikAU85in?filename=Global-politics_English.mp3">English Audio</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmX8aYwXcHBVGyiRd5ypWFL1s3g6caPPjndRiNwFuF24S9?filename=Global-politics_Burmese.mp3">Burmese Audio</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmbCYoZUe5SPdKTF8x1uyAAMXfKDXaX3ZwyerbANyfE68P?filename=Global-politics_English.pdf">English Transcript</a></p>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmQQ7qUkUqiCV23UsohoHK9oJGUwKDXB3r5XVhCiM3v5fw?filename=Global-politics_Burmese.pdf">Burmese Transcript</a></p>
+            <h5 class="i1">Readings</h5>
+              <p class="i2"><a href="https://ipfs.io/ipfs/QmSWATnMm8pvJmtpUY1b5sVHVeB3MsL3kz7M6AB7Nri5cJ?filename=Heywood-2011.pdf">Heywood 2011, Global Politics Chapter 1</a></p>
             <h4 class="i1">Additional Course Materials</h4>
                 <p class="i2"><a href="https://ipfs.io/ipfs/QmayvJJN9SUYC5c4Bwe5bz7hiohhFDEGnJMqCbP4yE9Rs3?filename=Politics%20textbook%20units%201%20and%202.pdf">[Textbook] Politics: Chapters 1 and 2</a></p>
                 <p class="i2"><a href="https://ipfs.io/ipfs/QmR2vT3SyWJzQNfbYc1i9rLfejTFCRaVjtx4RpbUVEofD6?filename=Politics%20textbook%20unit%203.pdf">[Textbook] Politics: Chapter 3</a></p>
