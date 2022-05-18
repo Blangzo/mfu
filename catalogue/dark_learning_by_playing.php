ff5940db32f64b10bf11e6a683ddd2c3d46881d5
ho_we <ho_wesley@yahoo.com>
2022-05-17 23:33:21 -0400

    added learning by playing 12-24months (modules 9-17)

--- catalogue/dark_learning_by_playing.php
+++ catalogue/dark_learning_by_playing.php
@@ -36,59 +36,7 @@
   </div>
   <div class="cat_body">
   <hr />
-
-  <div class="mb-4 mt-4">
-      <div class="mb-2 mt-3">
-        <h3 class="course">Learning by Playing - ကစားရင်းဖွံ့ဖြိုး၊ ဖွံ့ဖြိုးရင်းကစား</h3>
-          <h5 class="i1">Course Guide for 0-12 months (modules 1-8)</h5>
-            <p class="i2">12-36 months not yet ready</p>
-            <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Learning_by_playing_0-12.pdf">Guidelines for Learning by Playing course</a></p>
-          <h4 class="module">Module 1</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_1/LbyP_L1.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_1/Transcript_L1.pdf">Transcript</a></p>
-          <h4 class="module">Module 2</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_2/LbyP_L2.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_2/Transcript_L2.pdf">Transcript</a></p>
-          <h4 class="module">Module 3</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_3/LbyP_L3.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_3/Transcript_L3.pdf">Transcript</a></p>
-          <h4 class="module">Module 4</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_4/LbyP_L4.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_4/Transcript_L4.pdf">Transcript</a></p>
-
-          <h4 class="module">Module 5</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_5/LbyP_L5.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_5/Transcript_L5.pdf">Transcript</a></p>
-
-          <h4 class="module">Module 6</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_6/LbyP_L6.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_6/Transcript_L6.pdf">Transcript</a></p>
-
-          <h4 class="module">Module 7</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_7/LbyP_L7.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_7/Transcript_L7.pdf">Transcript</a></p>
-
-          <h4 class="module">Module 8</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_8/LbyP_L8.mp4">Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmaZmKuAyajhoqreeSoTtLjXFLBYoJXDPu2WSDtPGPmF96/Lecture_8/Transcript_L8.pdf">Transcript</a></p>
-      </div>
-  </div>
+ <?php include './learning_by_playing_include.php';?>
   <form class="course">
     <button onclick="location.href='../catalogue.php'" type="button">Back to catalogue</button>
   </form>
