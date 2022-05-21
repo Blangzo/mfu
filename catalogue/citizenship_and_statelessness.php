ab521dac27a1bfb099a30a4659218ba82fa21a0d
ho_we <ho_wesley@yahoo.com>
2022-05-20 21:21:10 -0400

    added modules 5 and 6 citizenship and statelessness

--- catalogue/citizenship_and_statelessness.php
+++ catalogue/citizenship_and_statelessness.php
@@ -36,56 +36,7 @@
   </div>
   <div class="cat_body">
   <hr />
-
-  <div class="mb-4 mt-4">
-      <div class="mb-2 mt-3">
-        <h3 class="course">Citizenship and Statelessness by Natalie Brinham, Institute of Statelessness and Inclusion </h3>
-          <h5 class="i1">Course Abstract</h5>
-            <p class="i2">This course will provide an overview of citizenship and statelessness in international contexts.
-            Learning materials will be developed by the Institute on Statelessness and Inclusion. It is an entry level course that will provide an introduction to the issues. For those looking to extend their learning
-            on these topics, we will provide links for materials that explore the issues in greater depth. We will
-            utilise existing resources but will target lessons specifically to a multi-ethnic Myanmar audience and
-            invite reflection on the relevance of the issues to the Myanmar context. The lessons will encourage
-            critical thinking and reflection. They will cover an introduction to statelessness; citizenship and
-            discrimination; childhood statelessness; citizenship stripping; statelessness and forced migration;
-            and access to citizenship in Myanmar. Most learning materials will be in English. Where possible we
-            will provide written and listening materials in Burmese. Following the release of each lesson and a
-            self-study period, we will also be available for discussion sessions with the students. We also
-            encourage peer-to-peer discussion of the lessons.</p>
-          <h5 class="i1">
-            <a href="https://ipfs.io/ipfs/QmeyY1SGaqBHYQksbMgpxAoBhruGUccJPkB26fi4fbKMJU?filename=cc_course_guide_en.docx">Citizenship and Statelessness Course Guide</a>
-          </h5>
-          <h4 class="module">Module 1</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/Qme3oTs8T9VjjwtjmNP1wx6oJo8dxCqQ2Y98egU6c8xCzR?filename=Lesson1_audio_introduction_to_citizenship_and_statelessness.mp3">English Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmV1DUQ2wwSXZNmszSwJkJiC8oFdzMsgfAmbuSRqVQZS8h?filename=Lesson1_transcript_citizenship_and_statelessness_en.pdf">English Transcript</a></p>
-            <h5 class="i1">Exercise</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmdszxXCWh6tFaVGwQk3BGhWEC2iRCVHK86HDhYfU7hTwh?filename=lesson1_introdction_citizenship_and_statelessness_exercise.docx">Lesson 1 Exercise</a></p>
-          <h4 class="module">Module 2</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmeMBEnpAawjscjkpLN3F3FwEdRCNrHSvhqje9y6Kxp9no?filename=Lesson2_audio_introduction_to_citizenship_and_statelessness.mp3">English Audio</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmPFwrscFtnLDVJJSXpDX6FzQwABLSdjXwzFdiEMhQjCWH?filename=Lesson2_transcript_citizenship_and_statelessness.docx">English Transcript</a></p>
-            <h5 class="i1">Case Study</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmUjUYNQDiBEE7RDQ57q8kufWzShYtZjKmRyb51hunRrZw?filename=lesson2_introdction_citizenship_and_statelessness_case_study.pdf">Lesson 2 Case Study</a></p>
-            <h5 class="i1">Exercise</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmSHbTFRH6uNQmgnQTZK2Pjx3cSXZqT5CTA1BC4esqGZSw?filename=lesson2_introdction_citizenship_and_statelessness_exercise_en.pdf">Lesson 2 Exercise</a></p>
-          <h4 class="module">Module 3</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmQGqd7HFCWnwoMvgiWtpAKp97FtdQBLqfZMoM4TqqCwPp?filename=Lesson3_audio1_childhood_statelessness.mp3">English Audio, Part 1</a></p>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmT98zCwk87HvHCXQGLw6T53zn4v5qqXvnxWrrkebR3YdN?filename=Lesson3_audio3_childhood_statelessness.mp3">English Audio, Part 3</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmUTV7JwMiZZrGSrWBz2kn1qtEXscc78bTrxAKccRzWGWo?filename=Lesson3_transcript_childhood_statelessness.pdf">English Transcript (Part 1)</a></p>
-            <h5 class="i1">Exercise</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmVrpWt993WRBMaWekqTVocWorF1G9zNiAsgFAuzwPfCp9?filename=lesson3_childhood_statelessness_with_logos_exercise.docx">Lesson 3 Exercise</a></p>
-          <h4 class="module">Module 4</h4>
-            <h5 class="i1">Lecture</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmXhWYyLBgCxn3nwjf5md6YgUxVCnoLbhFSYGLELgVK2oa?filename=Lesson1_audio2_childhood_statelessness.mp3">English Audio, Part 1</a></p>
-            <h5 class="i1">Transcript</h5>
-              <p class="i2"><a href="https://ipfs.io/ipfs/QmP7N6Wzgg6wUjgSW5qLuCASHUxE18XN5mNsmjKuE7NtYj?filename=Lesson3_transcript_childhood_statelessness.pdf">English Transcript (Part 1)</a></p>
-      </div>
-  </div>
+<?php include './citizenship_and_statelessness_include.php';?>
   <form class="course">
     <button onclick="location.href='../catalogue.php'" type="button">Back to catalogue</button>
   </form>
