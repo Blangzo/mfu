f2bb22744aabe6f29b35397de009b83bd2bf66ad
Wesley Ho <wh395@cornell.edu>
2022-08-11 22:49:47 -0400

    added intro to federalism course in  burmese

---
+++ catalogue/dark_intro_to_comparative_federalism.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="my">
+
+<head>
+  <title>ဖက်ဒရယ် တက္ကသိုလ်</title>
+  <link rel="icon" sizes="16x16 32x32 64x64" href="vfulogoonly.ico"/>
+  <meta name="VFU" content="VFU" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php include 'includes/google_analytics_tag.php';?>
+  <!--<link rel="stylesheet" type="text/css" href="styles/v1.css"/>-->
+  <link rel="stylesheet" type="text/css" href="../styles/v2.css"/>
+  <link rel="stylesheet" href="../lib/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../lib/bootstrap/dist/css/open-iconic-bootstrap.min.css" />
+  <!-- <link rel="stylesheet" href="lib/DataTables/datatables.min.css" /> -->
+  <link rel="stylesheet" href="../styles/dark-mode/site.css" class="dark_css"/>
+  <link rel="stylesheet" href="../styles/dark-mode/efa.css" class="dark_css"/>
+  <link rel="stylesheet" type="text/css" href="../styles/topnav.css"/>
+  <link rel="stylesheet" type="text/css" href="../styles/dark-mode/catalogue.css" class="dark_css"/>
+</head>
+
+<body>
+  <?php include 'includes/google_tag_manager_body.php';?>
+  <div class="topnav">
+    <a href="../dark_index.php">Home</a>
+    <a class="active" href="../dark_catalogue.php">Catalogue</a>
+    <a href="../dark_contact.php">Contact</a>
+    <a href="../dark_technical.php">Technical</a>
+    <a class="mode" href="intro_to_comparative_federalism.php" id="light_mode">Light Mode</a>
+    <a href="dark_intro_to_comparative_federalism.php" id="dark_mode" class="active mode">Dark Mode</a>
+  </div>
+
+  <div class="mb-2 mt-2">
+      <h2 class="title-color mb-3 text-center">ဖက်ဒရယ် တက္ကသိုလ်</h2>
+      <h2 class="title-color mb-2 text-center">Virtual Federal University</h2>
+  </div>
+  <hr />
+ <?php include './intro_to_comparative_federalism_include.php';?>
+    <form class="course">
+      <button onclick="location.href='../catalogue.php'" type="button">Back to catalogue</button>
+    </form>
+  </body>
+  </html>
---
+++ catalogue/intro_to_comparative_federalism_inclue.php
@@ -0,0 +1,107 @@
+  <div class="mb-4 mt-4">
+      <div class="mb-2 mt-3">
+        <h3 class="course">Introduction to Comparative Federalism by Thiha Wint Aung</h3>
+          <h4 class="i1">Note: Course currently only available in Burmese</h4>
+
+          <h4 class="module">Lecture 1: Introduction to federalism</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmeXiubqbWPzuFhRVHwgV67xmqzrdLmVVfVsVCQitTUz3K">Burmese Audio - Introduction to federalism</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/Qmd8K537J7hm5judpTrcfjH3YGVyDszJZWdLMqLVpGrCJm">Burmese Transcript - Introduction to federalism</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmbJyCkJZqkGxBjoHZvqDu7fa6nDdueYuPNggfy69NpB2V">Anderson, George. 2008. Federalism: An Introduction. Don Mills,  Ont.: Oxford University Press. Chapter 1 & 2</a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmTtAFRE8YGM6LnwZnsc8bng3AeBzkRahpwdRuGiK7nZRm">Watts, Ronald L. 2008. Comparing Federal Systems. 3rd ed. Queen’s Policy Studies. Montréal. Chapter 1.</a></p>
+
+          <h4 class="module">Lecture 2: Democracy and Patterns of Democracy</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmXTxWQjfbkCAF9BqtJyUVkT68kGWdRp1vQY7pqCvnf428">Burmese Audio - Democracy and Patterns of Democracy</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmdqBuAhj923n7F9xz8SYhNe5E94WcdAypCCyQodDzxUWV">Burmese Transcript - Democracy and Patterns of Democracy</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmaCLi8LgQMEzc1ajFSH2S4Qzyf1YewxbShi18MYs8hBDT">Schmitter, Philippe C, and Terry Lynn Karl. “What Democracy Is. . . and Is Not.” Journal of Democracy 2, no. 3 (1991)  75–88..pdf </a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmbjhdwhsDry6pw86Gn3VEnDWwgqFiATYgGUsMypEbi3Q4">Government Forms and Performance in Thirty-Six Countries. New Haven, Conn.  Yale Univ. Press, Second edition, 2012. Chapter 1, 2 & 3..pdf </a></p>
+
+          <h4 class="module">Lecture 3: Nation Building, Unity, and Diversity</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmbRFZvS8foFEX5N6tsUpXGpsvqQ5CW56JdVeErBD2GePV">Burmese Audio - Nation Building, Unity, and Diversity</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmfLhdd8mZqbdvYt6FPapRuux8kyMDyfYU9pymUfQvdXxZ">Burmese Transcript - Nation Building, Unity, and Diversity</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmSqYz54uW44Wgivat5NmY2qmL2hvwsWUn3ZbEvCXUcz6K">Htet Min Lwin. “Federalism at the Forefront of Myanmar’s Revolution.” Kyoto Review, no. 31 (2021) </a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmciM2t5ECQSdZkJoq43YyFstJKPXGYWi1jVZKKqc8vscX">Loh, Francis Kok Wah. “Ethnic Diversity and the Nation State  From Centralization in the Age of Nationalism to Decentralization amidst Globalization.” Inter-Asia Cultural Studies 18, no. 3 </a></p>
+
+          <h4 class="module">Lecture 4: Division of Power in a Federal System</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmamkQkPTEBMWrXeCzBgoXYysDyJZREHYYCfAar6qDr7rb">Burmese Audio - Division of Power in a Federal System</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/Qmc7z5zgXWp641Tv9KrWnnbgwP3u6d9ps16UfJX2FiDm86">Burmese Transcript - Division of Power in a Federal System</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2">Anderson, George. 2008. Federalism  An Introduction. Don Mills, Ont.  Oxford University Press. Chapter 4 (See Lecture 1)'s download </a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/link">Watts, Ronald L. 2008. Comparing Federal Systems. 3rd ed.  Queen’s Policy Studies. Montréal. Chapter 5 (Use Lecture 1's download) </a></p>
+
+          <h4 class="module">Lecture 5: Constituent Units in a Federal System</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmZexhmg6fBZ83fL3VsUy3bhjhWLFDXUdcpftiLnhRqd5N">Burmese Audio - Constituent Units in a Federal System</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmVpr1ZSjcvmnYqS7Er8gfKbfTSzMHwuHw2TzrSaUnehRF">Burmese Transcript - Constituent Units in a Federal System</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2">Anderson, George. 2008. Federalism  An Introduction. Don Mills, Ont.  Oxford University Press. Chapter 4 (See Lecture 1)'s download </a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2">Watts, Ronald L. 2008. Comparing Federal Systems. 3rd ed.  Queen’s Policy Studies. Montréal. Chapter 3 (Use Lecture 1's download) </a></p>
+
+          <h4 class="module">Lecture 6: Local governments in a Federal System</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmaUNBBGPiqVnD5gwfxtqiQ15XaPtkSPJ3wSgPWq2UpgAU">Burmese Audio - Local governments in a Federal System</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmWrsPW6dt8FKh8CbjTAj7frdmCrKptLqMm4SMgubNvZFL">Burmese Transcript - Local governments in a Federal System</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmYpcJcb723vCxNSk4kc8CG8Yu3JW3ZcPhybCzV6Yjjm1u">Forum of Federations. 2019. “Governance of Federal Capital Cities in Federal Countries” in Federal Essays (translated by Htet Min Lwin). Pg. 1-32 </a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2">Forum of Federations. 2019. “Local Governments in a Federal System” in Federal Essays (translated by Htet Min Lwin). Pg. 33-48 (Same file as required reading) </a></p>
+
+          <h4 class="module">Lecture 7: Fiscal Federalism</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/Qme8ci9YDfwgmvppMembpvWC9Aj2GBpXta5Bv5CMar89qo">Burmese Audio - Fiscal Federalism</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmbTvhQ9zAPCjJuvETeHCpRxj9TGa6qfxpsupgrmiXpfXR">Burmese Transcript - Fiscal Federalism</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmQQYFa77Udav7UdsSiFemV2M37xGSLw7ykGPsZZvm2rpV">Anderson, George. Fiscal Federalism - A Comparative Introduction. Don Mills, Ont.   New York  Oxford University Press, 2010. Chapter 1 </a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmZhjYcBcUFHa7rz58ZWbyaBU7nZH3iEnsm47bN1VdHQNb">Tremblay, Jean-Francois. 2019. Fiscal Arrangements in Selected Federal Countries. 1st ed. Forum of Federations Myanmar Office </a></p>
+
+          <h4 class="module">Lecture 8: Resource Federalism</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/Qme9AA9EU3EJ8LbcBEtnvKpQm9mMrN2wWXqHnuAFrNbaZh">Burmese Audio - Resource Federalism</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmTLQ9qqBrocJS5rBvvkUdhta6tqHibRaqZTTkxHV7P5di">Burmese Transcript - Resource Federalism</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2">Forum of Federations. 2019. “Natural Resources” in Federal Essays (translated by Htet Min Lwin). Pg. 49-95</a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2">Anderson, George. Fiscal Federalism - A Comparative Introduction. Don Mills, Ont New York Oxford University Press, 2010. Chapter 1 (See Lecture 7's required reading) </a></p>
+
+          <h4 class="module">Lecture 9: Education and Healthcare in Federalism</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmbSZA2eEKxBJaEw2gqDJq2zcoF5mM2w2LmMGzQJcHRQNA">Burmese Audio - Education and Healthcare in Federalism</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmdGkBcAupH6ES9U6nq7hmwF1TSmUbo51kfQkofeUWB3XJ">Burmese Transcript - Education and Healthcare in Federalism </a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmbF3xR6zGsgXE5mDApUMKZpG65ZzJosGDL8R5v1zDShuN">Wong, Kenneth K., Felix Knüpling, Mario Kölling, and Diana Chebenova, eds. Federalism and Education  Ongoing Challenges and Policy Strategies  in Ten Countries. Chapter 1</a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmNPYE3XC6B3N4CHQ43ysibfzSSDxwmkrk989XYboFWUMD"> Bossert, Thomas J., and Gregory P. Marchildon, eds. Federalism and Decentralization in Health Care A Decision Space Approach. Toronto ; Buffalo ; London  University of  Toronto Press, 2018</a></p>
+              <p class="i2">Wong, Kenneth K., Felix Knüpling, Mario Kölling, and Diana Chebenova, eds. Federalism and Education Ongoing Challenges and Policy Strategies in Ten Countries. Chapter 8</a></p>
+
+          <h4 class="module">Lecture 1: Constitution and Federalism</h4>
+            <h5 class="i1">Lectures</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmQd6SRp75rzXwRtgix28hqA4QNyAWAesCNHvw11jT6D5Z">Burmese Audio - Constitution and Federalism</a></p>
+            <h5 class="i1">Transcripts</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmTc8sveoziUzT5WinuBgSMHujrZsVQiRpZATQtUHcSX4i">Burmese Transcript - Constitution and Federalism</a></p>
+            <h5 class="i1">Required Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/link">Watts, Ronald L. 2008. Comparing Federal Systems. 3rd ed. Queen’s Policy Studies. Montréal. Chapter 11 (See Lecture 1)</a></p>
+            <h5 class="i1">Optional Readings</h5>
+              <p class="i2"><a href="https://mfu.mypinata.cloud/ipfs/QmUunEiAC3SrmgbGsRqja8xDBPvBq4RoyAwQoeknwgszZG">Burgess, Michael., Tar, G. Alan., 2020. Federal Constitutions and sub-national constitutionalism</a></p>
+      </div>
+  </div>
